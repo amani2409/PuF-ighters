9e21bffb4c334b9c413e8e3e7c855e8a87827c74
Nakato-Illi <illi-nakato@hotmail.de>
2023-05-04 18:52:34 +0200

    nur design

--- src/main/java/com/example/pufighters/Controllers/HomepageController.java
+++ src/main/java/com/example/pufighters/Controllers/HomepageController.java
@@ -3,12 +3,15 @@ package com.example.pufighters.Controllers;
 import com.example.pufighters.Model.SwitchScene;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class HomepageController {
+public class HomepageController implements Initializable {
 
     @FXML
     private Button button_logout;
@@ -34,5 +37,14 @@ public class HomepageController {
         new SwitchScene(homepageAchorpane, "Fxml/settings.fxml");
     }
 
+    @FXML
+    void onClickTerminate(ActionEvent event) {
 
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
--- src/main/java/com/example/pufighters/Main.java
+++ src/main/java/com/example/pufighters/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Fxml/login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
         stage.setTitle("PuF-ighters!");
         stage.setScene(scene);
         stage.show();
