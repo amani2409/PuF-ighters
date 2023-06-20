23b09dcf9a63aaf66782726b5891ebfb43436d90
Nakato-Illi <illi-nakato@hotmail.de>
2023-06-20 17:47:42 +0200

    bye animation

--- src/main/java/com/example/pufighters/Model/Animation.java
+++ src/main/java/com/example/pufighters/Model/Animation.java
@@ -1,63 +1,137 @@
 package com.example.pufighters.Model;
 
 import javafx.animation.*;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
+import javafx.scene.shape.QuadCurveTo;
 import javafx.util.Duration;
 
-public class Animation {
+public class Animation{
     public ImageView fightFigure;
+
+    int leftToRight = 450;
+    int rightToLeft = -450;
+
+    int moveUp = 200;
+    int moveDown = -200;
 
     public Animation(ImageView fightFigure) {
         this.fightFigure = fightFigure;
     }
 
-    public static void tranlate(ImageView figure) {
+//    ------------------------- BASIS -------------------------------     //
+
+    public static TranslateTransition tranlate(ImageView figure, int directionX, int directionY) {
         TranslateTransition translate = new TranslateTransition();
         translate.setNode(figure);
         translate.setDuration(Duration.millis(1000));
         translate.setCycleCount(2);
-        translate.setByX(250);
+        translate.setByY(directionY);
+        translate.setByX(directionX);
         translate.setAutoReverse(true);
         translate.play();
+
+        return translate;
     }
 
-    public static void rotate(ImageView figure) {
+    public static RotateTransition rotate(ImageView figure) {
         RotateTransition rotate = new RotateTransition();
         rotate.setNode(figure);
         rotate.setDuration(Duration.millis(1000));
         rotate.setCycleCount(2);
         rotate.setInterpolator(Interpolator.LINEAR);
-        //-------------------normalangriff
-        rotate.setByAngle(350);
-        //-------------------Norm + Spezialangriff1
-        //rotate.setAxis(Rotate.X_AXIS);
-        //-------------------translate + vom Bildschirm rollen
-        //rotate.setAxis(Rotate.Z_AXIS);
-        //rotate.setAutoReverse(true);
+        rotate.setByAngle(1440);
         rotate.play();
+
+        return rotate;
     }
 
-    public static void fade(ImageView figure) {
+    public static FadeTransition fade(ImageView figure, double opacityFrom, double opacityTo) {
         FadeTransition fade = new FadeTransition();
         fade.setNode(figure);
         fade.setDuration(Duration.millis(1000));
         fade.setCycleCount(2);
-        fade.setFromValue(0);
-        fade.setToValue(1);
+        fade.setFromValue(opacityFrom);
+        fade.setToValue(opacityTo);
         fade.play();
+
+        return fade;
     }
 
-    public static void scale(ImageView figure) {
+    public static ScaleTransition scale(ImageView figure, double with, double height) {
         ScaleTransition scale = new ScaleTransition();
         scale.setNode(figure);
         scale.setDuration(Duration.millis(1000));
         scale.setCycleCount(2);
-        scale.setByX(2.0);
+        scale.setByX(with);
+        scale.setByY(height);
         scale.setAutoReverse(true);
         scale.play();
+
+        return scale;
     }
+
+    public static PathTransition arc(ImageView figure) {
+        Path arc = new Path();
+
+        arc.getElements().add(new MoveTo(10, 20));
+        arc.getElements().add(new QuadCurveTo(440, 40, 250, 340));
+
+        PathTransition arcPath = new PathTransition();
+        arcPath.setNode(figure);
+        arcPath.setDuration(Duration.millis(1000));
+        arcPath.setCycleCount(2);
+        arcPath.setPath(arc);
+        arcPath.setAutoReverse(true);
+        arcPath.play();
+
+        return arcPath;
+    }
+
+    //    ------------------------- !!! BASIS - Angirffe!!! -------------------------------     //
+
+//    TAKLE
+
+    public static void takleOrRetrete(ImageView fightFigure, int direction) {
+        tranlate(fightFigure, direction, 0);
+    }
+
+    public static void jumpOrDig(ImageView fightFigure, int direction) {
+        tranlate(fightFigure, 0, direction);
+    }
+
+    public static void walzer(ImageView fig, int directX) {
+        paraTrans2(tranlate( fig, directX,0 ), rotate(fig));
+    }
+
+    public static void balloon(ImageView fig, double with, double height) {
+        scale(fig, with, height);
+    }
+    public static void ghostBalloon(ImageView fig, double with, double height, double opacityFrom, double opacityTo) {
+        paraTrans2(scale( fig, with, height), fade(fig, opacityFrom, opacityTo));
+    }
+
+    public static void paraTrans2(Transition t_one, Transition t_two) {
+        ParallelTransition paraTrans = new ParallelTransition();
+        paraTrans.getChildren().addAll(t_one, t_two);
+    }
+
+    public static void paraTrans3(ImageView effect, Label mana, Transition t_one,Transition t_two, Transition t_three ) {
+        ParallelTransition paraTrans = new ParallelTransition();
+        paraTrans.getChildren().addAll(t_one, t_two, t_three, fade(effect, 1, .4 ));
+        mana.setPrefWidth(100);
+    }
+    public static void paraTrans4(ImageView effect, Label mana, Transition t_one,Transition t_two, Transition t_three , Transition t_four) {
+        ParallelTransition paraTrans = new ParallelTransition();
+        paraTrans.getChildren().addAll(t_one, t_two, t_three, t_four, fade(effect, 1, .4 ));
+        mana.setPrefWidth(50);
+    }
+
 
     public static void raincomet(int cometenZahel, AnchorPane aPane) {
         for (int i = 0; i < cometenZahel; i++) {
@@ -93,4 +167,8 @@ public class Animation {
         translate.setByX(100);
         translate.play();
     }
+
+
+
+
 }
